20177f114c9ff2c5d40505530b5fa110c226d9b9
Jérôme Martinez <jerome@mediaarea.net>
2019-11-15 13:11:47 +0100

    Command line parser fixes

--- Source/CLI/CLI_Help.cpp
+++ Source/CLI/CLI_Help.cpp
@@ -21,12 +21,12 @@ return_value Help(const char* Name, bool Full)
     "Usage: \"" << Name << " [-Options...] FileName1 [Filename2...]\"\n";
     if (!Full)
     {
-        cout << "\"" << Name << " --Help\" for displaying more information.\n";
+        cout << "\"" << Name << " --help\" for displaying more information.\n";
         return ReturnValue_OK;
     }
     cout << "\n"
     "Options:\n"
-    "--Help, -h         Display this help and exit.\n"
+    "--help, -h         Display this help and exit.\n"
     "--Version          Display DVRescue version and exit.\n"
     ;
     
